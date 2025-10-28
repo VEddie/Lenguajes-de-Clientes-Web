87fc6764eb68ec5d3477ba792f50f9dddab9cc36
VEddie <edijose18@gmail.com>
2025-10-27 22:18:23 -0400

    Added sum and subtract functions to click events

--- matrix_solver/js/matrix.js
+++ matrix_solver/js/matrix.js
@@ -1,18 +1,19 @@
 let test_a = [
     [3, 5, 9],
     [2, 4, 7],
-    [8, 3, 6],
+    [8, 3, 6]
 ];
 
 let test_b = [
     [3, 5, 7],
     [1, 9, 2],
-    [7, 1, 4],
+    [7, 1, 4]
 ];
+
 
 let createIdentityMatrix = (size) => {
     let result = [];
-    for(let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
         let row = new Array(size).fill(0);
         row[i] = 1;
         result.push(row);
@@ -21,180 +22,14 @@ let createIdentityMatrix = (size) => {
     return result;
 };
 
-let sumMatrices = (a, b) => {
-    let result = [];
-    for(let i = 0; i < a.length; i++) {
-        result.push([]);
-        for(let j = 0; j < a[i].length; j++) 
-            result[i].push(a[i][j] + b[i][j])
-    }
-    
-    console.log(result);      
-};
-
-let subtractMatrices = (a, b) => {
-    let result = [];
-    for(let i = 0; i < a.length; i++) {
-        result.push([]);
-        for(let j = 0; j < a[i].length; j++) 
-            result[i].push(a[i][j] - b[i][j])
-    }
-
-    console.log(result);      
-};
-
-let multiplyMatrices = (a, b) => {
-    let result = [];
-    for(let i = 0; i < a.length; i++) {
-        result.push([]);
-        for(let j = 0; j < a[i].length; j++) {
-            let sum = 0;
-            for(let k = 0; k < b[i].length; k++) {
-                sum += a[i][k] * b[k][j];
-            }
-            result[i].push(sum);
-        }
-    }
-
-    console.log(result);
-
-};
-
-let multiplyBy = (value, a) => {
-    let result = [];
-    for(let i = 0; i < a.length; i++) {
-        result.push([]);
-        for(let j = 0; j < a[i].length; j++)
-            result[i].push(value * a[i][j]);
-    }
-
-    console.log(result);
-};
-
-
-
-let transposeMatrix = (a) => {
-    let result = createIdentityMatrix(a.length);
-    for(let i = 0; i < a.length; i++) 
-        for(let j = 0; j < a[i].length; j++) 
-            result[j][i] = a[i][j];
-
-    console.log(result);
-};
-
-
-// Change variable names for clarity sake.
-
-let calculateDeterminant = (a) => {
-    for(let i = 0; i < (a.length - 1); i++) {
-        let currentRow = a[i];
-        let currentValue = a[i][i];
-        // console.log(`Current row: ${currentRow}`);
-        // console.log(`Current value: ${currentValue}`);
-
-        for(let j = (i+1); j < a.length; j++) {
-            let nextRow = a[j];
-            let targetValue = a[j][i];
-            let factor = -((targetValue)/currentValue);
-            // console.log(`Target value: ${targetValue}`);
-            // console.log(`Current factor: ${factor}`);
-
-            let values = currentRow.map(v => v * factor);
-            let addedValues = nextRow.map((v, index) => v + values[index]);
-            
-            a[j] = addedValues;
-            console.log(addedValues);
-            
-        }
-    }
-
-    let determinant = 1;
-    for(let i = 0; i < a.length; i++)
-        determinant *= a[i][i];
-
-    console.log(`The determinant is: ${determinant.toFixed(4)}`)
-};
-
-// calculateDeterminant(test_a)
-
-let createInverseMatrix = (a) => {
-    let result = createIdentityMatrix(a.length);
-
-    for(let i = 0; i < a.length; i++) {
-        let currentRow = a[i];
-        let currentValue = a[i][i];
-
-        // Can be combined
-        if(currentValue != 1) {
-            // Plus zero to fix a weird quirk with JS and negative zeroes.
-            let multipliedValues = currentRow.map(v => (v * (1/currentValue)) + 0);         
-            a[i] = multipliedValues;
-
-            // For inverse
-            result[i] = result[i].map(v => (v * (1/currentValue)));
-
-        }
-        
-        // Exit first set of loops.
-        if(i === (a.length - 1)) break;
-        
-        for(let j = (i+1); j < a.length; j++) {
-            let nextRow = a[j];
-            let targetValue = a[j][i];
-            let factor = -((targetValue)/currentValue);
-
-            let values = currentRow.map(v => v * factor);
-
-            let addedValues = nextRow.map((v, index) => v + values[index]);            
-            a[j] = addedValues;
-
-            // For inverse, add currentValue to the equation.
-            let invertedValues = result[i].map(v => (v * factor * currentValue) + 0);
-            result[j] = result[j].map((v, index) => v + invertedValues[index]);
-            
-        }
-    }
-
-    /* Matrix now looks like this:
-        [1, value, value]
-        [0,    1,  value]
-        [0,    0,    1  ]
-    */
-
-    for(let i = (a.length -1); i > 0; i--) {
-        let currentRow = a[i];
-        let currentValue = a[i][i];
-
-        for(let j = (i-1); j > -1; j--) {
-            let nextRow = a[j];
-            let targetValue = a[j][i];
-            let factor = -((targetValue)/currentValue);
-
-            let values = currentRow.map(v => v * factor);
-            let addedValues = nextRow.map((v, index) => v + values[index]);
-            
-            a[j] = addedValues;
-
-            // For inverse
-            let invertedValues = result[i].map(v => (v * factor * currentValue) + 0);
-            result[j] = result[j].map((v, index) => v + invertedValues[index]);
-        }
-    }
-
-    // console.log(a);
-    // console.log(result);
-};
-
-createInverseMatrix(test_a);
-
 let createMatrix = (array, size) => {
     let result = [];
 
-    if(array.length % size !== 0) return;
+    if (array.length % size !== 0) return;
 
-    for(let i = 0; i < array.length; i+= size) 
+    for (let i = 0; i < array.length; i += size)
         result.push(array.slice(i, i + size));
-    
+
     return result;
 };
 
@@ -203,9 +38,9 @@ let increaseMatrixSize = (matrix, targetSize) => {
     let newMatrix = window.structuredClone(matrix);
 
     // Fix this later.
-    for(let i = 0; i < targetSize - 1; i++) 
+    for (let i = 0; i < targetSize - 1; i++)
         newMatrix[i].push(0);
-    
+
     newMatrix.push(new Array(targetSize).fill(0));
 
     return newMatrix;
@@ -223,4 +58,168 @@ let decreaseMatrixSize = (matrix, targetSize) => {
     return newMatrix;
 };
 
-export { createMatrix, increaseMatrixSize, decreaseMatrixSize };
+let sumMatrices = (a, b) => {
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result.push([]);
+        for (let j = 0; j < a[i].length; j++)
+            result[i].push(a[i][j] + b[i][j])
+    }
+
+    return result;
+};
+
+let subtractMatrices = (a, b) => {
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result.push([]);
+        for (let j = 0; j < a[i].length; j++)
+            result[i].push(a[i][j] - b[i][j])
+    }
+
+    return result;
+};
+
+let multiplyMatrices = (a, b) => {
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result.push([]);
+        for (let j = 0; j < a[i].length; j++) {
+            let sum = 0;
+            for (let k = 0; k < b[i].length; k++) {
+                sum += a[i][k] * b[k][j];
+            }
+            result[i].push(sum);
+        }
+    }
+
+    return result;
+
+};
+
+let multiplyBy = (value, a) => {
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result.push([]);
+        for (let j = 0; j < a[i].length; j++)
+            result[i].push(value * a[i][j]);
+    }
+
+    return result;
+};
+
+
+
+let transposeMatrix = (a) => {
+    let result = createIdentityMatrix(a.length);
+    for (let i = 0; i < a.length; i++)
+        for (let j = 0; j < a[i].length; j++)
+            result[j][i] = a[i][j];
+
+    return result;
+};
+
+
+// Change variable names for clarity sake.
+
+let calculateDeterminant = (a) => {
+    for (let i = 0; i < (a.length - 1); i++) {
+        let currentRow = a[i];
+        let currentValue = a[i][i];
+        // console.log(`Current row: ${currentRow}`);
+        // console.log(`Current value: ${currentValue}`);
+
+        for (let j = (i + 1); j < a.length; j++) {
+            let nextRow = a[j];
+            let targetValue = a[j][i];
+            let factor = -((targetValue) / currentValue);
+            // console.log(`Target value: ${targetValue}`);
+            // console.log(`Current factor: ${factor}`);
+
+            let values = currentRow.map(v => v * factor);
+            let addedValues = nextRow.map((v, index) => v + values[index]);
+
+            a[j] = addedValues;
+            console.log(addedValues);
+
+        }
+    }
+
+    let determinant = 1;
+    for (let i = 0; i < a.length; i++)
+        determinant *= a[i][i];
+
+    return determinant.toFixed(4);
+};
+
+let createInverseMatrix = (a) => {
+    let result = createIdentityMatrix(a.length);
+
+    for (let i = 0; i < a.length; i++) {
+        let currentRow = a[i];
+        let currentValue = a[i][i];
+
+        // Can be combined
+        if (currentValue != 1) {
+            // Plus zero to fix a weird quirk with JS and negative zeroes.
+            let multipliedValues = currentRow.map(v => (v * (1 / currentValue)) + 0);
+            a[i] = multipliedValues;
+
+            // For inverse
+            result[i] = result[i].map(v => (v * (1 / currentValue)));
+
+        }
+
+        // Exit first set of loops.
+        if (i === (a.length - 1)) break;
+
+        for (let j = (i + 1); j < a.length; j++) {
+            let nextRow = a[j];
+            let targetValue = a[j][i];
+            let factor = -((targetValue) / currentValue);
+
+            let values = currentRow.map(v => v * factor);
+
+            let addedValues = nextRow.map((v, index) => v + values[index]);
+            a[j] = addedValues;
+
+            // For inverse, add currentValue to the equation.
+            let invertedValues = result[i].map(v => (v * factor * currentValue) + 0);
+            result[j] = result[j].map((v, index) => v + invertedValues[index]);
+
+        }
+    }
+
+    /* Matrix now looks like this:
+        [1, value, value]
+        [0,    1,  value]
+        [0,    0,    1  ]
+    */
+
+    for (let i = (a.length - 1); i > 0; i--) {
+        let currentRow = a[i];
+        let currentValue = a[i][i];
+
+        for (let j = (i - 1); j > -1; j--) {
+            let nextRow = a[j];
+            let targetValue = a[j][i];
+            let factor = -((targetValue) / currentValue);
+
+            let values = currentRow.map(v => v * factor);
+            let addedValues = nextRow.map((v, index) => v + values[index]);
+
+            a[j] = addedValues;
+
+            // For inverse
+            let invertedValues = result[i].map(v => (v * factor * currentValue) + 0);
+            result[j] = result[j].map((v, index) => v + invertedValues[index]);
+        }
+    }
+
+    // console.log(a);
+    // console.log(result);
+    return result;
+};
+
+
+export { createIdentityMatrix, createMatrix, increaseMatrixSize, decreaseMatrixSize, sumMatrices, subtractMatrices, multiplyMatrices, multiplyBy, transposeMatrix, calculateDeterminant, createInverseMatrix };
--- matrix_solver/js/script.js
+++ matrix_solver/js/script.js
@@ -1,10 +1,23 @@
-import { createMatrix, increaseMatrixSize, decreaseMatrixSize } from "./matrix.js";
+import {
+    createIdentityMatrix,
+    createMatrix,
+    increaseMatrixSize,
+    decreaseMatrixSize,
+    sumMatrices,
+    subtractMatrices,
+    multiplyMatrices,
+    multiplyBy,
+    transposeMatrix,
+    calculateDeterminant,
+    createInverseMatrix
+}
+    from "./matrix.js";
 
 /* TO DO: 
-    - Add every function from matrix.js.
-    - Refactor some of the matrix.js functions for optimization.
-    - Add a function that display a result from any operation.
-    - Make it look prettier, for goodness sake.
+    - Add every function from matrix.js. []
+    - Refactor some of the matrix.js functions for optimization. []
+    - Add a function that displays a result from any operation. []
+    - Make it look prettier, for goodness sake. []
 */
 
 let matrix_a = [
@@ -27,6 +40,7 @@ let inputCount = 9;
 
 // Spread operator forces HTMLCollection to an array.
 let matrices = [...document.getElementsByClassName('matrix')];
+let results = document.querySelector('.results');
 
 let increaseButton = document.getElementById('increase');
 let decreaseButton = document.getElementById('decrease');
@@ -42,57 +56,73 @@ let determinantButton = document.getElementById('determinant');
 let inverseButton = document.getElementById('inverse');
 let identityButton = document.getElementById('identity');
 
+let loadResult = (matrix) => {
+    let div = document.createElement('div');
+    div.className = 'matrix';
+    div.style.gridTemplateColumns = `repeat(${matrix.length}, 1fr)`;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+            let input = document.createElement('input');
+            input.type = 'number';
+            input.value = matrix[i][j];
+            input.disabled = true;
+            
+            div.appendChild(input);
+        }
+    }
+
+    return div;
+};
+
 let loadMatrices = (operation) => {
-    for(let i = 0; i < matrices.length; i++) {
+    for (let i = 0; i < matrices.length; i++) {
         let inputArray = createMatrix([...matrices[i].children], currentSize);
 
         // Load values from inputValues
-        for(let j = 0; j < currentSize; j++) {
-            for(let k = 0; k < currentSize; k++) {
-                if(operation === 'clear') 
+        for (let j = 0; j < currentSize; j++) {
+            for (let k = 0; k < currentSize; k++) {
+                if (operation === 'clear')
                     inputArray[j][k].value = 0;
-                
-                else if(operation === 'fill')
+
+                else if (operation === 'fill')
                     inputArray[j][k].value = Math.floor((Math.random() - 0.5) * 20);
-                
-                else 
-                    inputArray[j][k].value = inputValues[i][j][k]; 
-            } 
+
+                else
+                    inputArray[j][k].value = inputValues[i][j][k];
+            }
         }
     }
 }
 
 let updateMatrices = () => {
-    for(let i = 0; i < matrices.length; i++) {
-        let inputArray = [...matrices[i].children].map(input => input.value);
+    for (let i = 0; i < matrices.length; i++) {
+        let inputArray = [...matrices[i].children].map(input => parseFloat(input.value));
         let inputMatrix = createMatrix(inputArray, currentSize);
 
         // Loop the input fields and update the matrix_a and matrix_b values.
-        for(let j = 0; j < currentSize; j++) 
-            for(let k = 0; k < currentSize; k++)  
+        for (let j = 0; j < currentSize; j++)
+            for (let k = 0; k < currentSize; k++)
                 inputValues[i][j][k] = inputMatrix[j][k];
     }
-
-    console.log('inputValues');
-    console.log(inputValues);
 }
 
 increaseButton.addEventListener('click', () => {
-    if(currentSize === 10) {
+    if (currentSize === 10) {
         alert('Máximo tamaño permitido es 10x10.');
         return;
     }
 
     // Grab current values.
     updateMatrices();
-    
+
     currentSize++;
 
     // Add new input fields.
-    for(let i = 0; i < matrices.length; i++) {
+    for (let i = 0; i < matrices.length; i++) {
         matrices[i].style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
 
-        for(let j = inputCount; j < (currentSize ** 2); j++) {
+        for (let j = inputCount; j < (currentSize ** 2); j++) {
             let input = document.createElement('input');
             input.type = 'number';
             input.value = 0;
@@ -102,7 +132,7 @@ increaseButton.addEventListener('click', () => {
         inputValues[i] = increaseMatrixSize(inputValues[i], currentSize);
         console.log(inputValues);
     }
-    
+
     inputCount = (currentSize ** 2);
 
     // Load current values, maintaining their order.
@@ -111,35 +141,64 @@ increaseButton.addEventListener('click', () => {
 });
 
 decreaseButton.addEventListener('click', () => {
-    if(currentSize === 2) {
+    if (currentSize === 2) {
         alert('Mínimo tamaño permitido es 2x2.');
         return;
     }
 
     updateMatrices();
     currentSize--;
-    
-    for(let i = 0; i < matrices.length; i++) {
+
+    for (let i = 0; i < matrices.length; i++) {
         matrices[i].style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
 
-        for(let j = inputCount; j > (currentSize ** 2); j--) 
+        for (let j = inputCount; j > (currentSize ** 2); j--)
             matrices[i].removeChild(matrices[i].firstElementChild);
 
         inputValues[i] = decreaseMatrixSize(inputValues[i], currentSize);
     }
 
-    
+
     inputCount = (currentSize ** 2);
     loadMatrices();
-    
+
 });
 
 fillButton.addEventListener('click', () => {
     loadMatrices('fill');
+    updateMatrices();
 })
 
 clearButton.addEventListener('click', () => {
     loadMatrices('clear');
+    updateMatrices();
 });
+
+sumButton.addEventListener('click', () => {    
+    updateMatrices();
+
+    let paragraphResult = document.createElement('p');
+    paragraphResult.textContent = 'Sum result:';
+
+    let sumMatrix = sumMatrices(inputValues[0], inputValues[1]);
+    let result = loadResult(sumMatrix);
+    
+    results.appendChild(paragraphResult);
+    results.appendChild(result);
+});
+
+subtractButton.addEventListener('click', () => {    
+    updateMatrices();
+
+    let paragraphResult = document.createElement('p');
+    paragraphResult.textContent = 'Substraction result:';
+
+    let subtractMatrix = subtractMatrices(inputValues[0], inputValues[1]);
+    let result = loadResult(subtractMatrix);
+    
+    results.appendChild(paragraphResult);
+    results.appendChild(result);
+});
+
 
 window.addEventListener('load', loadMatrices);
